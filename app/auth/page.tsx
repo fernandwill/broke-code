06f521bb992a96604374b322b500983212453c7e
Fernand William <156734117+fernandwill@users.noreply.github.com>
2025-11-23 13:04:22 +0700

    feat: hero image implemented

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -9,6 +9,9 @@ const AuthPage:React.FC<AuthPageProps> = () => {
     return <div className="relative min-h-screen bg-gradient-to-b from-gray-600 to-black">
         <div className="max-w-7xl mx-auto">
             <Navbar />
+            <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-event-none select-none">
+                <img src="/hero.png" alt="Hero.img" />
+            </div>
         </div>
     </div>
 }
