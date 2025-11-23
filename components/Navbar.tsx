e8fa1962b7726222046a9b4ee8c0617184212c9d
Fernand William <156734117+fernandwill@users.noreply.github.com>
2025-11-23 12:53:06 +0700

    feat: navbar designed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import Link from "next/link";
 
 type NavbarProps = {};
 
 const Navbar: React.FC<NavbarProps> = () => {
-    return <div>Navbar</div>;
+    return <div className="flex items-center justify-between sm:px-12 px-2 md:px-24">
+        <Link href="/" className="flex items-center justify-center h-20">
+            <img src="/logo.png" alt="LeetClone" className="h-full" />
+        </Link>
+        <div className="flex-items-center">
+            <button className="bg-[#FF7A00] text-white font-semibold py-2 px-4 rounded-full mr-4 border-2 border-transparent hover:text-[#FF7A00] hover:bg-white hover:border-2 hover:border-[#FF7A00]transition duration-500 ease-in-out">Sign in</button>
+        </div>
+    </div>;
 }
 
 export default Navbar;
